95d873307230b791f64a2cd9bdfd83f03568cbd5
Alessandro Olivares <alessandro.olivares@utp.ac.pa>
2021-11-08 17:45:35 -0500

    inicio

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,114 +23,114 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
-        Me.imgPresentacion = New System.Windows.Forms.PictureBox()
-        Me.labelTitulo = New System.Windows.Forms.Label()
-        Me.grupoSeleccion = New System.Windows.Forms.GroupBox()
-        Me.radioAlquiler = New System.Windows.Forms.RadioButton()
-        Me.radioReservacion = New System.Windows.Forms.RadioButton()
-        Me.radioPrecio = New System.Windows.Forms.RadioButton()
-        Me.botonContinuar = New System.Windows.Forms.Button()
-        Me.botonCancelar = New System.Windows.Forms.Button()
-        CType(Me.imgPresentacion, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.grupoSeleccion.SuspendLayout()
+        Me.ImgPresentacion = New System.Windows.Forms.PictureBox()
+        Me.LabelTitulo = New System.Windows.Forms.Label()
+        Me.GrupoSeleccion = New System.Windows.Forms.GroupBox()
+        Me.RadioAlquiler = New System.Windows.Forms.RadioButton()
+        Me.RadioReservacion = New System.Windows.Forms.RadioButton()
+        Me.RadioPrecio = New System.Windows.Forms.RadioButton()
+        Me.BotonContinuar = New System.Windows.Forms.Button()
+        Me.BotonCancelar = New System.Windows.Forms.Button()
+        CType(Me.ImgPresentacion, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GrupoSeleccion.SuspendLayout()
         Me.SuspendLayout()
         '
-        'imgPresentacion
+        'ImgPresentacion
         '
-        Me.imgPresentacion.Image = CType(resources.GetObject("imgPresentacion.Image"), System.Drawing.Image)
-        Me.imgPresentacion.Location = New System.Drawing.Point(75, 100)
-        Me.imgPresentacion.Name = "imgPresentacion"
-        Me.imgPresentacion.Size = New System.Drawing.Size(350, 200)
-        Me.imgPresentacion.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.imgPresentacion.TabIndex = 0
-        Me.imgPresentacion.TabStop = False
+        Me.ImgPresentacion.Image = CType(resources.GetObject("ImgPresentacion.Image"), System.Drawing.Image)
+        Me.ImgPresentacion.Location = New System.Drawing.Point(75, 100)
+        Me.ImgPresentacion.Name = "ImgPresentacion"
+        Me.ImgPresentacion.Size = New System.Drawing.Size(350, 200)
+        Me.ImgPresentacion.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.ImgPresentacion.TabIndex = 0
+        Me.ImgPresentacion.TabStop = False
         '
-        'labelTitulo
+        'LabelTitulo
         '
-        Me.labelTitulo.Cursor = System.Windows.Forms.Cursors.Default
-        Me.labelTitulo.Dock = System.Windows.Forms.DockStyle.Top
-        Me.labelTitulo.Enabled = False
-        Me.labelTitulo.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.labelTitulo.Font = New System.Drawing.Font("Candara", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.labelTitulo.Location = New System.Drawing.Point(0, 0)
-        Me.labelTitulo.Margin = New System.Windows.Forms.Padding(2)
-        Me.labelTitulo.Name = "labelTitulo"
-        Me.labelTitulo.Padding = New System.Windows.Forms.Padding(0, 15, 0, 15)
-        Me.labelTitulo.RightToLeft = System.Windows.Forms.RightToLeft.Yes
-        Me.labelTitulo.Size = New System.Drawing.Size(884, 90)
-        Me.labelTitulo.TabIndex = 1
-        Me.labelTitulo.Text = "PANAMA CAR RENTAL"
-        Me.labelTitulo.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.LabelTitulo.Cursor = System.Windows.Forms.Cursors.Default
+        Me.LabelTitulo.Dock = System.Windows.Forms.DockStyle.Top
+        Me.LabelTitulo.Enabled = False
+        Me.LabelTitulo.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.LabelTitulo.Font = New System.Drawing.Font("Candara", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelTitulo.Location = New System.Drawing.Point(0, 0)
+        Me.LabelTitulo.Margin = New System.Windows.Forms.Padding(2)
+        Me.LabelTitulo.Name = "LabelTitulo"
+        Me.LabelTitulo.Padding = New System.Windows.Forms.Padding(0, 15, 0, 15)
+        Me.LabelTitulo.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.LabelTitulo.Size = New System.Drawing.Size(884, 90)
+        Me.LabelTitulo.TabIndex = 1
+        Me.LabelTitulo.Text = "PANAMA CAR RENTAL"
+        Me.LabelTitulo.TextAlign = System.Drawing.ContentAlignment.TopCenter
         '
-        'grupoSeleccion
+        'GrupoSeleccion
         '
-        Me.grupoSeleccion.BackColor = System.Drawing.Color.White
-        Me.grupoSeleccion.Controls.Add(Me.radioAlquiler)
-        Me.grupoSeleccion.Controls.Add(Me.radioReservacion)
-        Me.grupoSeleccion.Controls.Add(Me.radioPrecio)
-        Me.grupoSeleccion.Location = New System.Drawing.Point(475, 100)
-        Me.grupoSeleccion.Name = "grupoSeleccion"
-        Me.grupoSeleccion.Size = New System.Drawing.Size(360, 200)
-        Me.grupoSeleccion.TabIndex = 2
-        Me.grupoSeleccion.TabStop = False
+        Me.GrupoSeleccion.BackColor = System.Drawing.Color.White
+        Me.GrupoSeleccion.Controls.Add(Me.RadioAlquiler)
+        Me.GrupoSeleccion.Controls.Add(Me.RadioReservacion)
+        Me.GrupoSeleccion.Controls.Add(Me.RadioPrecio)
+        Me.GrupoSeleccion.Location = New System.Drawing.Point(475, 100)
+        Me.GrupoSeleccion.Name = "GrupoSeleccion"
+        Me.GrupoSeleccion.Size = New System.Drawing.Size(360, 200)
+        Me.GrupoSeleccion.TabIndex = 2
+        Me.GrupoSeleccion.TabStop = False
         '
-        'radioAlquiler
+        'RadioAlquiler
         '
-        Me.radioAlquiler.BackColor = System.Drawing.Color.Transparent
-        Me.radioAlquiler.Font = New System.Drawing.Font("Candara", 14.25!, System.Drawing.FontStyle.Bold)
-        Me.radioAlquiler.Location = New System.Drawing.Point(35, 131)
-        Me.radioAlquiler.Name = "radioAlquiler"
-        Me.radioAlquiler.Size = New System.Drawing.Size(300, 50)
-        Me.radioAlquiler.TabIndex = 2
-        Me.radioAlquiler.TabStop = True
-        Me.radioAlquiler.Text = "ALQUILER"
-        Me.radioAlquiler.UseVisualStyleBackColor = False
+        Me.RadioAlquiler.BackColor = System.Drawing.Color.Transparent
+        Me.RadioAlquiler.Font = New System.Drawing.Font("Candara", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.RadioAlquiler.Location = New System.Drawing.Point(35, 131)
+        Me.RadioAlquiler.Name = "RadioAlquiler"
+        Me.RadioAlquiler.Size = New System.Drawing.Size(300, 50)
+        Me.RadioAlquiler.TabIndex = 2
+        Me.RadioAlquiler.TabStop = True
+        Me.RadioAlquiler.Text = "ALQUILER"
+        Me.RadioAlquiler.UseVisualStyleBackColor = False
         '
-        'radioReservacion
+        'RadioReservacion
         '
-        Me.radioReservacion.BackColor = System.Drawing.Color.Transparent
-        Me.radioReservacion.Font = New System.Drawing.Font("Candara", 14.25!, System.Drawing.FontStyle.Bold)
-        Me.radioReservacion.Location = New System.Drawing.Point(35, 75)
-        Me.radioReservacion.Name = "radioReservacion"
-        Me.radioReservacion.Size = New System.Drawing.Size(300, 50)
-        Me.radioReservacion.TabIndex = 1
-        Me.radioReservacion.TabStop = True
-        Me.radioReservacion.Text = "RESERVACIÓN"
-        Me.radioReservacion.UseVisualStyleBackColor = False
+        Me.RadioReservacion.BackColor = System.Drawing.Color.Transparent
+        Me.RadioReservacion.Font = New System.Drawing.Font("Candara", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.RadioReservacion.Location = New System.Drawing.Point(35, 75)
+        Me.RadioReservacion.Name = "RadioReservacion"
+        Me.RadioReservacion.Size = New System.Drawing.Size(300, 50)
+        Me.RadioReservacion.TabIndex = 1
+        Me.RadioReservacion.TabStop = True
+        Me.RadioReservacion.Text = "RESERVACIÓN"
+        Me.RadioReservacion.UseVisualStyleBackColor = False
         '
-        'radioPrecio
+        'RadioPrecio
         '
-        Me.radioPrecio.BackColor = System.Drawing.Color.Transparent
-        Me.radioPrecio.Font = New System.Drawing.Font("Candara", 14.25!, System.Drawing.FontStyle.Bold)
-        Me.radioPrecio.Location = New System.Drawing.Point(35, 19)
-        Me.radioPrecio.Name = "radioPrecio"
-        Me.radioPrecio.Size = New System.Drawing.Size(300, 50)
-        Me.radioPrecio.TabIndex = 0
-        Me.radioPrecio.TabStop = True
-        Me.radioPrecio.Text = "PRECIOS"
-        Me.radioPrecio.UseVisualStyleBackColor = False
+        Me.RadioPrecio.BackColor = System.Drawing.Color.Transparent
+        Me.RadioPrecio.Font = New System.Drawing.Font("Candara", 14.25!, System.Drawing.FontStyle.Bold)
+        Me.RadioPrecio.Location = New System.Drawing.Point(35, 19)
+        Me.RadioPrecio.Name = "RadioPrecio"
+        Me.RadioPrecio.Size = New System.Drawing.Size(300, 50)
+        Me.RadioPrecio.TabIndex = 0
+        Me.RadioPrecio.TabStop = True
+        Me.RadioPrecio.Text = "PRECIOS"
+        Me.RadioPrecio.UseVisualStyleBackColor = False
         '
-        'botonContinuar
+        'BotonContinuar
         '
-        Me.botonContinuar.BackColor = System.Drawing.Color.White
-        Me.botonContinuar.Font = New System.Drawing.Font("Candara", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.botonContinuar.Location = New System.Drawing.Point(500, 350)
-        Me.botonContinuar.Name = "botonContinuar"
-        Me.botonContinuar.Size = New System.Drawing.Size(300, 100)
-        Me.botonContinuar.TabIndex = 3
-        Me.botonContinuar.Text = "&CONTINUAR"
-        Me.botonContinuar.UseVisualStyleBackColor = False
+        Me.BotonContinuar.BackColor = System.Drawing.Color.White
+        Me.BotonContinuar.Font = New System.Drawing.Font("Candara", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BotonContinuar.Location = New System.Drawing.Point(500, 350)
+        Me.BotonContinuar.Name = "BotonContinuar"
+        Me.BotonContinuar.Size = New System.Drawing.Size(300, 100)
+        Me.BotonContinuar.TabIndex = 3
+        Me.BotonContinuar.Text = "&CONTINUAR"
+        Me.BotonContinuar.UseVisualStyleBackColor = False
         '
-        'botonCancelar
+        'BotonCancelar
         '
-        Me.botonCancelar.BackColor = System.Drawing.Color.White
-        Me.botonCancelar.Font = New System.Drawing.Font("Candara", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.botonCancelar.Location = New System.Drawing.Point(100, 350)
-        Me.botonCancelar.Name = "botonCancelar"
-        Me.botonCancelar.Size = New System.Drawing.Size(300, 100)
-        Me.botonCancelar.TabIndex = 4
-        Me.botonCancelar.Text = "C&ANCELAR"
-        Me.botonCancelar.UseVisualStyleBackColor = False
+        Me.BotonCancelar.BackColor = System.Drawing.Color.White
+        Me.BotonCancelar.Font = New System.Drawing.Font("Candara", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BotonCancelar.Location = New System.Drawing.Point(100, 350)
+        Me.BotonCancelar.Name = "BotonCancelar"
+        Me.BotonCancelar.Size = New System.Drawing.Size(300, 100)
+        Me.BotonCancelar.TabIndex = 4
+        Me.BotonCancelar.Text = "C&ANCELAR"
+        Me.BotonCancelar.UseVisualStyleBackColor = False
         '
         'Form1
         '
@@ -138,26 +138,26 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Silver
         Me.ClientSize = New System.Drawing.Size(884, 561)
-        Me.Controls.Add(Me.botonCancelar)
-        Me.Controls.Add(Me.botonContinuar)
-        Me.Controls.Add(Me.grupoSeleccion)
-        Me.Controls.Add(Me.imgPresentacion)
-        Me.Controls.Add(Me.labelTitulo)
+        Me.Controls.Add(Me.BotonCancelar)
+        Me.Controls.Add(Me.BotonContinuar)
+        Me.Controls.Add(Me.GrupoSeleccion)
+        Me.Controls.Add(Me.ImgPresentacion)
+        Me.Controls.Add(Me.LabelTitulo)
         Me.Name = "Form1"
         Me.Text = "PANAMA CAR RENTAL"
         Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
-        CType(Me.imgPresentacion, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.grupoSeleccion.ResumeLayout(False)
+        CType(Me.ImgPresentacion, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GrupoSeleccion.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents imgPresentacion As PictureBox
-    Friend WithEvents grupoSeleccion As GroupBox
-    Friend WithEvents radioAlquiler As RadioButton
-    Friend WithEvents radioReservacion As RadioButton
-    Friend WithEvents radioPrecio As RadioButton
-    Friend WithEvents botonContinuar As Button
-    Friend WithEvents botonCancelar As Button
-    Friend WithEvents labelTitulo As Label
+    Friend WithEvents ImgPresentacion As PictureBox
+    Friend WithEvents GrupoSeleccion As GroupBox
+    Friend WithEvents RadioAlquiler As RadioButton
+    Friend WithEvents RadioReservacion As RadioButton
+    Friend WithEvents RadioPrecio As RadioButton
+    Friend WithEvents BotonContinuar As Button
+    Friend WithEvents BotonCancelar As Button
+    Friend WithEvents LabelTitulo As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -2,32 +2,4 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
-
-    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles imgPresentacion.Click
-
-    End Sub
-
-
-    Private Sub rbtprecio_CheckedChanged(sender As Object, e As EventArgs) Handles radioPrecio.CheckedChanged
-        Form2.Show()
-
-
-    End Sub
-
-    Private Sub btncancelar_Click(sender As Object, e As EventArgs) Handles botonCancelar.Click
-        Me.Close()
-
-    End Sub
-
-    Private Sub goveleccion_Enter(sender As Object, e As EventArgs) Handles grupoSeleccion.Enter
-        radioAlquiler.Checked = True
-    End Sub
-
-    Private Sub btnavanzar_Click(sender As Object, e As EventArgs) Handles botonContinuar.Click
-
-    End Sub
-
-    Private Sub lblTitulo_Click(sender As Object, e As EventArgs) Handles labelTitulo.Click
-
-    End Sub
 End Class
