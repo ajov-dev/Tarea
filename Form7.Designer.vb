af3779e84b7894a461564fe6ddc4b4d46c602380
utpAlessandro0330 <93617639+utpAlessandro0330@users.noreply.github.com>
2021-12-03 02:50:28 -0500

    Esta es para ustedes Aficion, SIIIIIIIIUU

---
+++ Form7.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form7
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LabelAviso = New System.Windows.Forms.Label()
+        Me.BotonCancelar = New System.Windows.Forms.Button()
+        Me.BotonContinuar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'LabelAviso
+        '
+        Me.LabelAviso.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.LabelAviso.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.LabelAviso.Font = New System.Drawing.Font("Candara", 15.75!, System.Drawing.FontStyle.Bold)
+        Me.LabelAviso.Location = New System.Drawing.Point(0, 0)
+        Me.LabelAviso.Name = "LabelAviso"
+        Me.LabelAviso.Size = New System.Drawing.Size(434, 261)
+        Me.LabelAviso.TabIndex = 0
+        Me.LabelAviso.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'BotonCancelar
+        '
+        Me.BotonCancelar.BackColor = System.Drawing.Color.FromArgb(CType(CType(80, Byte), Integer), CType(CType(109, Byte), Integer), CType(CType(132, Byte), Integer))
+        Me.BotonCancelar.Font = New System.Drawing.Font("Candara", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BotonCancelar.ForeColor = System.Drawing.Color.White
+        Me.BotonCancelar.Location = New System.Drawing.Point(12, 199)
+        Me.BotonCancelar.Name = "BotonCancelar"
+        Me.BotonCancelar.Size = New System.Drawing.Size(200, 50)
+        Me.BotonCancelar.TabIndex = 54
+        Me.BotonCancelar.Text = "R&EGRESAR"
+        Me.BotonCancelar.UseVisualStyleBackColor = False
+        '
+        'BotonContinuar
+        '
+        Me.BotonContinuar.BackColor = System.Drawing.Color.FromArgb(CType(CType(80, Byte), Integer), CType(CType(109, Byte), Integer), CType(CType(132, Byte), Integer))
+        Me.BotonContinuar.Font = New System.Drawing.Font("Candara", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BotonContinuar.ForeColor = System.Drawing.Color.White
+        Me.BotonContinuar.Location = New System.Drawing.Point(222, 199)
+        Me.BotonContinuar.Name = "BotonContinuar"
+        Me.BotonContinuar.Size = New System.Drawing.Size(200, 50)
+        Me.BotonContinuar.TabIndex = 53
+        Me.BotonContinuar.Text = "&SALIR"
+        Me.BotonContinuar.UseVisualStyleBackColor = False
+        '
+        'Form7
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.ClientSize = New System.Drawing.Size(434, 261)
+        Me.Controls.Add(Me.BotonCancelar)
+        Me.Controls.Add(Me.BotonContinuar)
+        Me.Controls.Add(Me.LabelAviso)
+        Me.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Name = "Form7"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Verificacion de Pago"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents LabelAviso As Label
+    Friend WithEvents BotonCancelar As Button
+    Friend WithEvents BotonContinuar As Button
+End Class
